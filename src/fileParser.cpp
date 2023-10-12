34c6321cb4a34fff91d6e4e9e38382d70d93537e
Avan Sharma <meadow.tulips@gmail.com>
2023-10-12 12:48:09 +0530

    Added information about number of tables

--- src/fileParser.cpp
+++ src/fileParser.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
+#include <cstring>
+#include <endian.h>
 #include "fileParser.h"
+
+unsigned int ToHex(char x)
+{
+    return (0xff & (unsigned int)x);
+}
+void getDbInfo(std::ifstream &ifs)
+{
+    char buffer[2]{'\0'};
+    short pageSize;
+    short btreeHeaderSize = 0;
+    ifs.read((char *)&pageSize, 2);
+    ifs.seekg(100, std::ios_base::beg);
+    memset(buffer, '\0', 2);
+    ifs.read(buffer, 1);
+
+    if ((ToHex(buffer[0]) == 0x02) || (ToHex(buffer[0]) == 0x05))
+    {
+        // Interior table BTree page
+        btreeHeaderSize = 12;
+    }
+    else if ((ToHex(buffer[0]) == 0x0A) || (ToHex(buffer[0]) == 0x0D))
+    {
+        // Leaf Index BTree page
+        btreeHeaderSize = 8;
+    }
+    else
+    {
+        // Not a bTree
+        return;
+    }
+    memset(buffer, '\0', 1);
+    ifs.seekg(2, std::ios::cur);
+    uint16_t countOfCells{0};
+    ifs.read((char *)&countOfCells, 2);
+
+    std::cout << "database page size: " << pageSize * 256 << std::endl;
+    std::cout << "number of tables: " << be16toh(countOfCells) << std::endl;
+}
 
 SQL_LITE::FileParser::FileParser(std::string fullFilePath) { filePath = fullFilePath; }
 
@@ -13,11 +53,9 @@ void SQL_LITE::FileParser::readFileAndExecuteCommand(std::string command)
 
         if (command == supported_commands[0])
         {
-            char buffer[2];
-            short pageSize;
-            ifs.read((char *)&pageSize, 2);
-            // unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
-            std::cout << "database page size: " << pageSize * 256 << std::endl;
+            getDbInfo(ifs);
         }
+
+        ifs.close();
     }
 }
